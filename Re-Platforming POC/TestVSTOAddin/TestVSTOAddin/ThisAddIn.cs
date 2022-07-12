0369b420c77ef0fdf0aa7f7e86c2de4105ea3f59
Gaurav Budhiraja <gaurav.budhiraja@litera.com>
2022-07-12 16:22:28 +0530

    added

--- Re-Platforming POC/TestVSTOAddin/TestVSTOAddin/ThisAddIn.cs
+++ Re-Platforming POC/TestVSTOAddin/TestVSTOAddin/ThisAddIn.cs
@@ -1,17 +1,74 @@
-﻿using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Wordprocessing;
-using Microsoft.Office.Interop.Word;
-using VstoHelperLibrary;
+﻿using Microsoft.Office.Interop.Word;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Xml.Linq;
+using Document = Microsoft.Office.Interop.Word.Document;
 
 namespace TestVSTOAddin
 {
+    class BADocument
+    {
+        public Document Document { get; set; }
+    }
+
     public partial class ThisAddIn
     {
-        private SampleControl sampleControl;
-        private Microsoft.Office.Tools.CustomTaskPane customTaskPane;
+        //private readonly SampleControl sampleControl;
+        //private Microsoft.Office.Tools.CustomTaskPane customTaskPane;
 
-        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        private void ThisAddIn_Startup(object sender, EventArgs e)
         {
+            //string fileName = activeDocument.Name;
+            //string newfileName = Guid.NewGuid().ToString() + "_" + fileName;
+            //string path = @"C:\" + newfileName;
+            //activeDocument.SaveAs2(path);
+            //string path = @"C:\ReceivedDoc2.docx";
+            //byte[] bytes = File.ReadAllBytes(path);
+            //Document document = Application.Documents.Open(@"C:\NewDoc1.docx", null, true);
+            Document document = Application.Documents.Open(@"C:\Test.docx", null, true);
+            //var nullObj = null;
+            //Application.Quit();
+            XDocument xDocument = XDocument.Parse(document.WordOpenXML);
+            //string xmlRequestBody = xDocument.ToString();
+
+            //var app = document.Application;
+            //var ass = app.Assistant;
+            //sampleControl = new SampleControl();
+            //customTaskPane = CustomTaskPanes.Add(sampleControl, "Sample Control");
+            //customTaskPane.Visible = true;
+            using (var client = new HttpClient())
+            {
+                //BADocument bADocument = new BADocument() { Document = document };
+                // Setting Base address.  
+                client.BaseAddress = new Uri("https://localhost:7275/");
+                /*Document*/
+                //var json = JsonConvert.SerializeObject(bytes);
+                var json = JsonConvert.SerializeObject(xDocument);
+                //var json = JsonConvert.SerializeObject(document, new JsonSerializerSettings
+                //{
+                //    ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                //    PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+                //    ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor
+                //});
+
+                //var data = new StringContent(json, Encoding.UTF8, "application/json");
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+                // HTTP POST  
+                var response = client.PostAsync("api/startup/HasNullHeaderFooter", data).Result;
+
+                // Verification  
+                if (response.IsSuccessStatusCode)
+                {
+                    // Reading Response.  
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    //receivedResult = JsonConvert.DeserializeObject<bool>(result);
+                }
+            }
+
+
             //Application.Documents.Open(@"C:\Doc.docx",false);
             //sampleControl = new SampleControl();
             //customTaskPane = CustomTaskPanes.Add(sampleControl, "Sample Control");
@@ -20,10 +77,40 @@ namespace TestVSTOAddin
             //string receivedXml = string.Empty;
             //Application.Documents.Open(@"C:\Users\gaurav.budhiraja\OneDrive - Litera\Documents\Application Security.docx");
             //Application.Documents.Open(@"C:\Doc.docx");
-            Microsoft.Office.Interop.Word.Document activeDocument = Application.Documents.Open(@"C:\Doc.docx");
+            //Microsoft.Office.Interop.Word.Document activeDocument = Application.Documents.Open(@"C:\Doc.docx");
+            //activeDocument.WordOpenXML
             //Application.ActiveDocument.Close();
             //Application.Documents.Open(@"C:\Doc1.docx");
-            string fileName = activeDocument.FullName;
+            //string fileName = activeDocument.Name;
+            //string newfileName = Guid.NewGuid().ToString() + "_" + fileName;
+            //string path = @"C:\" + newfileName;
+            //activeDocument.SaveAs2(path);
+            //string path = @"C:\Doc.docx";
+            //byte[] bytes = File.ReadAllBytes(path);
+
+            //using (var client = new HttpClient())
+            //{
+            //    // Setting Base address.  
+            //    client.BaseAddress = new Uri("https://localhost:7275/");
+            //    /*Document*/
+            //    var json = JsonConvert.SerializeObject(bytes);
+            //    var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            //    // HTTP POST  
+            //    var response = client.PostAsync("api/startup/HasNullHeaderFooter", data).Result;
+
+            //    // Verification  
+            //    if (response.IsSuccessStatusCode)
+            //    {
+            //        // Reading Response.  
+            //        var result = response.Content.ReadAsStringAsync().Result;
+            //        //receivedResult = JsonConvert.DeserializeObject<bool>(result);
+            //    }
+            //}
+
+            // return receivedResult;
+
+            // activeDocument.Fil
             //activeDocument.Close();
             //using (WordprocessingDocument document = WordprocessingDocument.Open(fileName, true))
             //{
@@ -36,10 +123,10 @@ namespace TestVSTOAddin
             //    };
             //    receivedXml = HelperClass.AddParagraph(request);
             //}
-            Request request = new Request() { 
-                WordOpenXML = activeDocument.WordOpenXML 
-            };
-            string recievedXml = HelperClass.AddParagraph(request);
+            //Request request = new Request() { 
+            //    WordOpenXML = activeDocument.WordOpenXML 
+            //};
+            //string recievedXml = HelperClass.AddParagraph(request);
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
